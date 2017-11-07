feb9ba63b01b929643b8ea723baee37c4a8059a6
AlfianHidayatM <32305314+AlfianHidayatM@users.noreply.github.com>
2017-11-07 20:54:56 +0700

    Memasukan Menu

--- latihan1.cpp
+++ latihan1.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
 	int x,y,selisih;
-	
+	 menu:
+	 system("cls");
+		cout <<"===========================\n";
+		cout <<"Masukan Angka atau bilangan\n";
+		cout <<"===========================\n"<<endl;
 }
